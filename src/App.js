81c0f2fa562ebbc4e7f6c447575579931db5d341
Sarah Julia Gushef <sarahgushef@gmail.com>
2020-07-07 14:15:03 +0700

    Create PokemonDetails Page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Login from './pages/Login/Login';
-import Pokemons from './pages/Pokemons/Pokemons';
-import PokemonDetails from './pages/PokemonDetails/PokemonDetails';
+import Login from "./pages/Login/Login";
+import Pokemons from "./pages/Pokemons/Pokemons";
+import PokemonDetails from "./pages/PokemonDetails/PokemonDetails";
 
-import PrivateRoute from './helpers/PrivateRoute';
+import PrivateRoute from "./helpers/PrivateRoute";
 
 function App() {
     return (
         <Router>
             <Switch>
-                <Route exact path='/login'>
+                <Route exact path="/login">
                     <Login />
                 </Route>
 
-                <PrivateRoute exact path='/pokemons'>
+                <PrivateRoute exact path="/pokemons">
                     <Pokemons />
                 </PrivateRoute>
 
-                <PrivateRoute exact path='/pokemons/:name'>
-                    <Route children={PokemonDetails} />
+                <PrivateRoute exact path="/pokemons/:name">
+                    <PokemonDetails />
                 </PrivateRoute>
             </Switch>
         </Router>
